14ec1b575c2c0bfccfac5fece3f69650f58c3785
KP <kyleprock@verizon.net>
2022-04-12 17:34:27 -0500

    add react icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import {AiOutlineShoppingCart} from 'react-icons/ai'
 import {Link, Outlet} from 'react-router-dom'
 import './App.css';
 
 function App() {
+  const style = {fontSize: "2.5em", float: "right"}
   return (
     <div className="App">
       <div className='banner'>
         <h1>Store</h1>
+        <AiOutlineShoppingCart style={style}/>
       </div>
       <div className='section'>
         <ul>
